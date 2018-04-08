8919de4877df834105635f15fa4274f35d4b3e73
satZonTearthX <33574752+satZonTearthX@users.noreply.github.com>
2018-04-08 13:57:30 +0800

    Update infint.hpp

--- infint.hpp
+++ infint.hpp
@@ -19,15 +19,15 @@ public:
 	
 	Infint operator+(Infint& n);
 	Infint operator+(int num);
-	Infint operator+(string& num);
+	Infint operator+(string num);
 	
 	Infint operator-(Infint& n);
 	Infint operator-(int num);
-	Infint operator-(string& num);
+	Infint operator-(string num);
 	
 	Infint operator*(Infint& n);
 	Infint operator*(int num);
-	Infint operator*(string& num);
+	Infint operator*(string num);
 
 	
 	Infint& operator++();
@@ -37,19 +37,19 @@ public:
 	Infint operator--(int);
 	void operator+=(const Infint& n);
 	void operator+=(const int num);
-	void operator+=(const string& num);
+	void operator+=(const string num);
 	
 	void operator-=(const Infint& n);
 	void operator-=(const int num);
-	void operator-=(const string& num);
+	void operator-=(const string num);
 	
 	void operator*=(const Infint& n);
 	void operator*=(const int num);
-	void operator*=(const string& num);
+	void operator*=(const string num);
 	
 	void operator=(const Infint& b);
 	void operator=(const int num);
-	void operator=(const string& num);
+	void operator=(const string num);
 };
 Infint minusnum(Infint& n);
 bool bigger(Infint&n1, Infint&n2);
